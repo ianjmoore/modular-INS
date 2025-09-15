94d17d6d803697affc95771cd37fe765a5b57850
ianjmoore <ian.j.moore95@gmail.com>
2025-09-15 10:34:53 -0400

    More updates to CMakeLists.txt, config/, launch/, package.xml

--- launch/cv7_ins_mosaic_h_launch.py
+++ launch/cv7_ins_mosaic_h_launch.py
@@ -29,61 +29,61 @@ def generate_launch_description():
       }.items()
     ),
     
-    # Septentrio Mosaic H node
-    Node(
-      package='septentrio_gnss_driver',
-      executable='septentrio_gnss_driver_node',
-      output='both',
-      parameters=[_SEPT_CONFIG_FILE],
-      remappings=[
-        ('/navsatfix','/cv7_ins/ext/llh_position'),
-        ('/twist','/cv7_ins/ext/velocity_enu')
-      ]),
+    # # Septentrio Mosaic H node
+    # Node(
+    #   package='septentrio_gnss_driver',
+    #   executable='septentrio_gnss_driver_node',
+    #   output='both',
+    #   parameters=[_SEPT_CONFIG_FILE],
+    #   remappings=[
+    #     ('/navsatfix','/cv7_ins/ext/llh_position'),
+    #     ('/twist','/cv7_ins/ext/velocity_enu')
+    #   ]),
 
-    # Publish a static transform for where the our gps is mounted on base_link.
-    # You should replace this with actual transforms for where your aiding sensors are
-    Node(
-      package='tf2_ros',
-      executable='static_transform_publisher',
-      output='screen',
-      arguments=[
-          "--x", "0.15",
-          "--y", "-0.45",
-          "--z", "1.58",
-          "--roll", "0",
-          "--pitch", "0",
-          "--yaw", "0",
-          "--frame-id", "base_link",
-          "--child-frame-id", "gps"
-        ]
-    ),
+    # # Publish a static transform for where the our gps is mounted on base_link.
+    # # You should replace this with actual transforms for where your aiding sensors are
+    # Node(
+    #   package='tf2_ros',
+    #   executable='static_transform_publisher',
+    #   output='screen',
+    #   arguments=[
+    #       "--x", "0.15",
+    #       "--y", "-0.45",
+    #       "--z", "1.58",
+    #       "--roll", "0",
+    #       "--pitch", "0",
+    #       "--yaw", "0",
+    #       "--frame-id", "base_link",
+    #       "--child-frame-id", "gps"
+    #     ]
+    # ),
 
-    # Publish a static transform for where the CV7-INS is mounted on base_link.
-    # Unless the CV7-INS is mounted exactly at base_link, you should change this to be accurate to your setup
-    Node(
-      package='tf2_ros',
-      executable='static_transform_publisher',
-      output='screen',
-      arguments=[
-          "--x", "-0.37",
-          "--y", "-0.55",
-          "--z", "0.63",
-          "--roll", "0",
-          "--pitch", "0",
-          "--yaw", "0",
-          "--frame-id", "base_link",
-          "--child-frame-id", "cv7_ins_link"
-        ]
-    ),
+    # # Publish a static transform for where the CV7-INS is mounted on base_link.
+    # # Unless the CV7-INS is mounted exactly at base_link, you should change this to be accurate to your setup
+    # Node(
+    #   package='tf2_ros',
+    #   executable='static_transform_publisher',
+    #   output='screen',
+    #   arguments=[
+    #       "--x", "-0.37",
+    #       "--y", "-0.55",
+    #       "--z", "0.63",
+    #       "--roll", "0",
+    #       "--pitch", "0",
+    #       "--yaw", "0",
+    #       "--frame-id", "base_link",
+    #       "--child-frame-id", "cv7_ins_link"
+    #     ]
+    # ),
 
-    # Run rviz to view the state of the application
-    Node(
-      package='rviz2',
-      executable='rviz2',
-      output='screen',
-      arguments=[
-        '-d', _RVIZ_DISPLAY_FILE
-      ]
-    ),
+    # # Run rviz to view the state of the application
+    # Node(
+    #   package='rviz2',
+    #   executable='rviz2',
+    #   output='screen',
+    #   arguments=[
+    #     '-d', _RVIZ_DISPLAY_FILE
+    #   ]
+    # ),
   ])
 

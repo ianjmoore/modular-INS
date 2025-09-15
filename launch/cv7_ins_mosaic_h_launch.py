7fc6b75db0d28b331d3ab9fc381d30613fdbcf3b
ianjmoore <ian.j.moore95@gmail.com>
2025-09-15 19:50:21 -0400

    Modified launch file to follow ros2 conventions

--- launch/cv7_ins_mosaic_h_launch.py
+++ launch/cv7_ins_mosaic_h_launch.py
@@ -7,15 +7,18 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch_ros.actions import Node
 
+_PACKAGE_NAME = 'modular_ins'
+
 # Path to the launch files and directories that we will use
 _MICROSTRAIN_LAUNCH_FILE = os.path.join(ament_index_python.packages.get_package_share_directory('microstrain_inertial_driver'), 'launch', 'microstrain_launch.py')
 _CV7_INS_PARAMS_FILE = os.path.join(ament_index_python.packages.get_package_share_directory('modular_ins'), 'config', 'cv7_ins_mosaic_h.yml')
 _RVIZ_DISPLAY_FILE = os.path.join(ament_index_python.packages.get_package_share_directory('modular_ins'), 'config', 'display.rviz')
 
 _CONFIG_DIRECTORY = os.path.join(ament_index_python.packages.get_package_share_directory('modular_ins'), 'config')
-_SEPT_CONFIG_FILE = os.path.join(_CONFIG_DIRECTORY, 'mosaic_h.yml') #TODO check if needed
+_SEPT_CONFIG_FILE = os.path.join(_CONFIG_DIRECTORY, 'cv7_ins_mosaic_h.yml') #TODO check if needed
 
 def generate_launch_description():
+  print(f"sept params: {_SEPT_CONFIG_FILE}")
   return LaunchDescription([
     # Microstrain node
     IncludeLaunchDescription(
@@ -24,23 +27,24 @@ def generate_launch_description():
         'configure': 'true',
         'activate': 'true',
         'params_file': _CV7_INS_PARAMS_FILE,
-        'namespace': '/cv7_ins', #need to verify
-	      'debug': 'true',
+        'namespace': 'cv7_ins', #need to verify
+	        'debug': 'true',
       }.items()
     ),
     
-    # # Septentrio Mosaic H node
-    # Node(
-    #   package='septentrio_gnss_driver',
-    #   executable='septentrio_gnss_driver_node',
-    #   output='both',
-    #   parameters=[_SEPT_CONFIG_FILE],
-    #   remappings=[
-    #     ('/navsatfix','/cv7_ins/ext/llh_position'),
-    #     ('/twist','/cv7_ins/ext/velocity_enu')
-    #   ]),
+    # Septentrio Mosaic H node
+    Node(
+      package='septentrio_gnss_driver',
+      executable='septentrio_gnss_driver_node',
+      output='screen',
+      parameters=[_SEPT_CONFIG_FILE],
+      remappings=[
+        ('/navsatfix','cv7_ins/ext/llh_position'),
+        ('/twist','cv7_ins/ext/velocity_enu')
+      ]
+    ),
 
-    # # Publish a static transform for where the our gps is mounted on base_link.
+    # # Publish a static transform for where the GNSS antenna is mounted on base_link.
     # # You should replace this with actual transforms for where your aiding sensors are
     # Node(
     #   package='tf2_ros',
@@ -58,8 +62,8 @@ def generate_launch_description():
     #     ]
     # ),
 
-    # # Publish a static transform for where the CV7-INS is mounted on base_link.
-    # # Unless the CV7-INS is mounted exactly at base_link, you should change this to be accurate to your setup
+    # Publish a static transform for where the CV7-INS is mounted on base_link.
+    # Unless the CV7-INS is mounted exactly at base_link, you should change this to be accurate to your setup
     # Node(
     #   package='tf2_ros',
     #   executable='static_transform_publisher',
@@ -86,4 +90,3 @@ def generate_launch_description():
     #   ]
     # ),
   ])
-
